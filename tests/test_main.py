cc665adc15c38d4cb55f78a223da7277b9bc9b75
201910u1 <>
2019-12-23 18:07:14 +0900

    test_1を渡したら文字列1を返す - ゴール(検証)

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,5 +3,8 @@ import unittest
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_これは落ちるはず(self):
-        self.assertEqual(1, 2)
+    def test_1を渡したら文字列1を返す(self):
+        # 前準備
+        # 実行
+        # 検証
+        self.assertEqual('1', fizzbuzz.convert(1))
