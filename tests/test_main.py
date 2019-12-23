fd1aa4b1e564b987d218d047887d034a74f50c61
201910u1 <>
2019-12-23 18:42:38 +0900

    test_2を渡したら文字列2を返す(三角測量) - Refactoring

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,15 +5,10 @@ from apps.main import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
 
-    # 前準備
     fizzbuzz = fizzbuzz()
 
     def test_1を渡したら文字列1を返す(self):
-        # 実行
-        # 検証
         self.assertEqual('1', fizzbuzz.convert(1))
 
     def test_2を渡したら文字列2を返す(self):  # 三角測量(Triangulation)
-        # 実行
-        # 検証
         self.assertEqual('2', fizzbuzz.convert(2))
